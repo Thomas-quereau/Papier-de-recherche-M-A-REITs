1a4357ba67ac1d238a79b00894af95ba24dc4ba3
yorn <yornclaudel@gmail.com>
2021-06-04 17:17:07 +0200

    t test seems to be working

--- sanitize.r
+++ sanitize.r
@@ -1,8 +1,11 @@
 # importing libraries
 library(data.table, quietly = TRUE)
-
 #plotting
 library(ggplot2)
+#conversion to percent for printing
+library(scales)
+# library for latex tables
+library(xtable)
 
 # Clean the environment
 rm(list = ls(all = TRUE))
@@ -16,13 +19,25 @@ dealsTable = setDT(read.csv("./data/Deals Mémoire.csv"))
 
 usaDeals = dealsTable[Acquiror.Mid.Industry == "REITs"][Acquiror.Nation.of.Primary.Stock.Exchange == "United States"][as.Date(Date.Announced) > as.Date("2003-01-01")]
 
-# Creating a dir to save the data fully sanitized
-dir.create('./data_sanitized')
+# Reading the FTUNUS index
+ftunusfile = setDT(read.csv('./data/Price History_FTUNUS.csv'))
+ftunus = ftunusfile[grep("Exchange Date", ftunusfile[[1]]) + 2:nrow(ftunusfile), ]
+setnames(ftunus, colnames(ftunus), as.vector(as.matrix(ftunusfile[17])))
+setnames(ftunus, '%Chg', 'ftunus_chg')
+ftunus[, 'Exchange Date' := as.Date(ftunus$`Exchange Date`)]
+ftunusChange = ftunus[, c('Exchange Date', 'ftunus_chg')]
+#ftunusChange = Filter(function(x)x!="", ftunusChange)
+ftunusChange = na.omit(Filter(function(x)!all(is.na(x)), ftunusChange))
 
+# mkdir('./output/')
+# printAnalysis = function(deals, outputFolder)
+# {
+#   mkdir(outputFolder)
+# }
 
 
 # sanitizing acquirers
-files = dir("./data/Acquirers/", full.names = TRUE)
+files = dir("./data/Targets/", full.names = TRUE)
 acquirers = list()
 
 sheet = NULL
@@ -52,16 +67,6 @@ for (file in files)
 
 
 
-# Reading the FTUNUS index
-ftunusfile = setDT(read.csv('./data/Price History_FTUNUS.csv'))
-ftunus = ftunusfile[grep("Exchange Date", ftunusfile[[1]]) + 2:nrow(ftunusfile), ]
-setnames(ftunus, colnames(ftunus), as.vector(as.matrix(ftunusfile[17])))
-setnames(ftunus, '%Chg', 'ftunus_chg')
-ftunus[, 'Exchange Date' := as.Date(ftunus$`Exchange Date`)]
-
-ftunusChange = ftunus[, c('Exchange Date', 'ftunus_chg')]
-#ftunusChange = Filter(function(x)x!="", ftunusChange)
-ftunusChange = na.omit(Filter(function(x)!all(is.na(x)), ftunusChange))
 
 companies = list()
 for (deal in names(acquirers))
@@ -72,27 +77,27 @@ for (deal in names(acquirers))
   company[, "Exchange Date" := as.Date(company$`Exchange Date`)]
   setnames(company, '%Chg', "chg")
 
-  print(deal)
   test = company 
   company = merge(ftunusChange, company, by="Exchange Date")
   company[, chg := as.numeric(gsub(",", ".", chg))]
   company[, ftunus_chg := as.numeric(gsub(",", ".", ftunus_chg))]
-  company[,AR := chg - ftunus_chg]
+  company[, AR := chg - ftunus_chg]
   
   reg = lm(chg ~ ftunus_chg, company[between(day, -110, -10)])
   a = reg$coef[("Intercept")]
-  b = reg$coef["Rm"]
+  b = reg$coef["ftunus_chg"]
+  # print(reg)
   company[, CAPM_AR := chg - b * ftunus_chg]
   companies[[deal]] =  company
 }
 
-getDayAAR = function(d, companies)
+getDayAAR = function(d, companies, field)
 {
   sum = 0
   for (company in companies)
   {
-    tmp = company[day == d, "AR"]
-    if (dim(tmp)[1] > 0 && !is.na(tmp) && length(tmp) != 0)
+    tmp = company[day == d][[field]]
+    if (!is.na(tmp) && length(tmp) != 0)
       sum = sum + tmp
   }
   return(sum / length(companies))
@@ -101,12 +106,67 @@ getDayAAR = function(d, companies)
 getAverageTable = function(companies)
 {
   t = data.table(day = -10:10)
-  t[, AAR := sapply(day, getDayAAR, companies)]
+  t[, ZEROR := sapply(day, getDayAAR, companies, "chg")]
+  t[, C_ZEROR := cumsum(ZEROR)]
+  t[, AAR := sapply(day, getDayAAR, companies, "AR")]
   t[, CAAR := cumsum(AAR)]
+  t[, CAPM_AAR := sapply(day, getDayAAR, companies, "CAPM_AR")]
+  t[, CAPM_CAAR := cumsum(CAPM_AAR)]
   return (t)
 }
 
+getSum = function(b, a, table, field)
+{
+  return(sum(table[between(day, a, b)][[field]]))
+}
+
+getCAARTables = function(table, fields)
+{
+  r = list()
+  for (name in fields)
+  {
+    t = (data.table(
+      "a = -5" = sapply(0:5, getSum, a = -5, table, name),
+      "a = -4" = sapply(0:5, getSum, a = -4, table, name),
+      "a = -3" = sapply(0:5, getSum, a = -3, table, name),
+      "a = -2" = sapply(0:5, getSum, a = -2, table, name),
+      "a = -1" = sapply(0:5, getSum, a = -1, table, name),
+      "a = -0" = sapply(0:5, getSum, a = +0, table, name)))
+    rownames(t) = paste0("b = ", 0:5)
+    r[[name]] = t
+  }
+  return (r)
+}
+
+
+getTestTable = function(table)
+{
+  test_table = table
+  
+  test_table[,AAR := round(AAR * 100, 2)]
+  test_table[,CAAR := round(CAAR * 100, 2)]
+  # tAAR
+  test_table[,tAAR := round(AAR/sd(AAR), 4)]
+  test_table[,mask := (1 - pt(tAAR, length(AAR))) < 0.05]
+  stars = factor(test_table$mask, labels=c("", "*"))
+  test_table[,tAAR := paste0(as.character(tAAR), as.character(stars))]
+  
+  # tCAAR
+  test_table[,tCAAR := round(CAAR/sd(CAAR), 4)]
+  test_table[,mask := (1 - pt(tCAAR, length(AAR))) < 0.05]
+  stars = factor(test_table$mask, labels=c("","*"))
+  test_table[,tCAAR := paste0(as.character(tCAAR), as.character(stars))]
+  
+  test_table[,mask := NULL] 
+  
+  test_table[,AAR := paste0(AAR, "%")]
+  test_table[,CAAR := paste0(CAAR, "%")]
+  return(test_table)
+}
+
 AARTable = getAverageTable(companies)
+IntervalTables = getCAARTables(AARTable, c("ZEROR","AAR", "CAPM_AAR"))
+test_table = getTestTable(AARTable)
 
 gg = ggplot(data=AARTable, aes(x = day, y = AAR)) +
  geom_vline(xintercept=0, colour="firebrick", size=1) +
