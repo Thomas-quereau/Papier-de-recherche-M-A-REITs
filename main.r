cdd00f4bd0265ceb1e67f3129b8bc075257a01eb
yorn <yornclaudel@gmail.com>
2021-06-08 00:47:59 +0200

    working on diagrams

--- main.r
+++ main.r
@@ -31,13 +31,15 @@ Q4 = getAnalysis(usaTargetsDeals[quartile == 4,], ftunusData, acquirers, targets
 
 quartileAnalysis = data.table(Quartile=c("25%","50%", "75%", "100%"),
                               "Rank value (millions)"= quantile(usaTargetsDeals$valuation)[2:5],
-                              CAPM_CAAR = c(
+                              "CAPM CAAR" = c(
                                 sum(Q1[["targets"]][["average_table"]][day >= -5 & day <= 5, CAPM_AAR]),
                                 sum(Q2[["targets"]][["average_table"]][day >= -5 & day <= 5, CAPM_AAR]),
                                 sum(Q3[["targets"]][["average_table"]][day >= -5 & day <= 5, CAPM_AAR]),
                                 sum(Q4[["targets"]][["average_table"]][day >= -5 & day <= 5, CAPM_AAR])
                             ))
-quartileAnalysis[,("Adjusted") := CAPM_CAAR - sum(usaDealsAnalysis[["targets"]][["average_table"]][day >= -5 & day <= 5, CAPM_AAR])]
+quartileAnalysis[,("Adjusted") := quartileAnalysis[["CAPM CAAR"]] - sum(usaDealsAnalysis[["targets"]][["average_table"]][day >= -5 & day <= 5, CAPM_AAR])]
+quartileAnalysis[,"CAPM CAAR" := paste0(round(quartileAnalysis[["CAPM CAAR"]] * 100,4), "%")]
+quartileAnalysis[,"Adjusted" := paste0(round(quartileAnalysis[["Adjusted"]] * 100,4), "%")]
 print(quartileAnalysis)
 
 printAnalysis(usaDealsAnalysis, "./output/usa_deals")
@@ -45,13 +47,22 @@ printAnalysis(Q1, './output/Q1')
 printAnalysis(Q2, './output/Q2')
 printAnalysis(Q3, './output/Q3')
 printAnalysis(Q4, './output/Q4')
-  print(xtable(
-    quartileAnalysis,
-    caption = "CAPM CAAR for the 4 tragets's quartile by valuation",
-    type = "latex"),
-    table.placement = "H",
-    caption.placement = "top",
-    include.rownames = FALSE,
-    floating = TRUE, latex.environments = "center",
-    file = "output/target_quartiles.tex")
-# printAnalysis(usa_tagets_analysis, './output/USA_targets')
+print(xtable(
+  quartileAnalysis,
+  caption = "CAPM CAAR for the 4 tragets's quartile by valuation",
+  type = "latex"),
+  table.placement = "H",
+  caption.placement = "top",
+  include.rownames = FALSE,
+  floating = TRUE, latex.environments = "center",
+  file = "output/target_quartiles.tex")
+
+targetsData = usaDealsAnalysis[["targets"]][["average_table"]][day <= 10 & day >= -50, c("day", "CAPM_AAR")]
+targetsData[, "CAPM CAAR" := cumsum(CAPM_AAR)]
+runups = ggplot(data = targetsData, aes(x=day, y=`CAPM CAAR`)) + geom_col(size=1.5) 
+print(runups)
+
+targetsData = usaDealsAnalysis[["acquirers"]][["average_table"]][day <= 10 & day >= -50, c("day", "CAPM_AAR")]
+targetsData[, "CAPM CAAR" := cumsum(CAPM_AAR)]
+runups = ggplot(data = targetsData, aes(x=day, y=`CAPM CAAR`)) + geom_col(size=1.5) 
+print(runups)
